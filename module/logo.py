06e9b5b42aa0fad004b55e1049896218c5e53252
uthree <uthree271828@gmail.com>
2023-10-07 18:50:20 +0900

    add logo function

---
+++ module/logo.py
@@ -0,0 +1,7 @@
+def print_logo():
+    print(" ________      _______  | ")
+    print("|___  /\ \    / / ____| | The voice conversion system")
+    print("   / /  \ \  / / |      | repository: https://github.com/uthree/zvc")
+    print("  / /    \ \/ /| |      | ")
+    print(" / /__    \  / | |____  | ")
+    print("/_____|    \/   \_____| | ")
