9783089929cd7da3c9fb7d9da4685df82db809b6
uthree <uthree271828@gmail.com>
2023-10-11 00:01:10 +0900

    Update inference script

--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ import torchaudio
 import os
 import glob
 import torch
+import torch.nn.functional as F
 from tqdm import tqdm
 
 from module.spectrogram import spectrogram
@@ -29,7 +30,7 @@ parser.add_argument('-d', '--device', default='cpu')
 parser.add_argument('-g', '--gain', default=1.0, type=float)
 parser.add_argument('-a', '--alpha', default=0.0, type=float)
 parser.add_argument('-k', default=4, type=int)
-parser.add_argument('-c', '--chunk', default=131072, type=int)
+parser.add_argument('-c', '--chunk', default=65536, type=int)
 parser.add_argument('-lib', '--voice-library-path', default="NONE")
 
 args = parser.parse_args()
@@ -71,13 +72,23 @@ for i, path in enumerate(paths):
     wf = wf.to('cpu')
     wf = torchaudio.functional.resample(wf, sr, 16000)
     wf = wf / wf.abs().max()
-    wf = wf[:1]
+    wf = wf.mean(dim=0, keepdim=True)
     total_length = wf.shape[1]
-    chunks = torch.split(wf, args.chunk, dim=1)
+    
+    if total_length < args.chunk:
+        wf = torch.cat([wf, torch.zeros(1, args.chunk-total_length)], dim=1)
+
+    wf = wf.unsqueeze(1).unsqueeze(1)
+    wf = F.pad(wf, (args.chunk, args.chunk, 0, 0))
+    chunks = F.unfold(wf, (1, args.chunk*3), stride=args.chunk)
+    chunks = chunks.transpose(1, 2).split(1, dim=1)
+
     result = []
     with torch.no_grad():
         print(f"converting {path}")
         for chunk in tqdm(chunks):
+            chunk = chunk.squeeze(1)
+
             if chunk.shape[1] < args.chunk:
                 chunk = torch.cat([chunk, torch.zeros(1, args.chunk - chunk.shape[1])], dim=1)
             chunk = chunk.to(device)
@@ -97,6 +108,9 @@ for i, path in enumerate(paths):
             feat = CE(spec)
             feat = match_features(feat, tgt, k=args.k, alpha=args.alpha)
             chunk = Dec.decode(feat, f0  * args.f0_rate)
+            
+            chunk = chunk[:, args.chunk:-args.chunk]
+
             result.append(chunk.to('cpu'))
         wf = torch.cat(result, dim=1)[:, :total_length]
         wf = torchaudio.functional.resample(wf, 16000, sr)
