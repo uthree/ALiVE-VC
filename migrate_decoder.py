ed904338100914548dca335a56327bc6a231d2e6
uthree <uthree271828@gmail.com>
2023-10-27 22:45:45 +0900

    add last normalization

--- migrate_decoder.py
+++
@@ -1,8 +0,0 @@
-from module.decoder import Decoder
-from module.common import AdaptiveChannelNorm
-import torch
-
-decoder = Decoder()
-decoder.load_state_dict(torch.load('./decoder.pt', map_location='cpu'))
-decoder.last_norm = AdaptiveChannelNorm(512, 512)
-torch.save(decoder.state_dict(), './decoder.pt')
