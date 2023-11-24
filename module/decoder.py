ee23bc402d598772e1f0ac310babc30656d50e31
uthree <uthree271828@gmail.com>
2023-11-25 03:19:36 +0900

    fix

--- module/decoder.py
+++ module/decoder.py
@@ -162,7 +162,7 @@ class PostFilter(nn.Module):
         x = self.input_layer(x)
         x = self.mid_layer(x)
         x = self.output_layer(x)
-        return x * (1 - alpha) + res * alpha
+        return x + res
 
 
 class Decoder(nn.Module):
